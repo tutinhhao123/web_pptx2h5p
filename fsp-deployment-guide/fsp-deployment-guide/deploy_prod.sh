84065692247f7cb9825900b6d6a6cd96852d81ea
tutinhhao123 <tutinhhao@gmail.com>
2024-03-18 13:59:13 +0700

    'ok'

--- fsp-deployment-guide/fsp-deployment-guide/deploy_prod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ansible-playbook ./prod/deploy.yml --private-key=\
-./ssh_keys/prod_key -K -u deployer -i ./prod/hosts
--- fsp-deployment-guide/fsp-deployment-guide/ssh_keys/note.md
+++
@@ -1,2 +0,0 @@
-Your public-private keypair and authorized_keys file should be created and
-stored in this directory.
